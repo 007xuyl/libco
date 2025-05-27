d50aa87343e4ebc9d84eac39063a5a956a4decfd
xuyaolan <oncwnuFspHH0MhlBCvqOjDJD1rXs@git.weixin.qq.com>
2025-05-27 16:00:21 +0800

    test rabbit

--- example_thread.cpp
+++ example_thread.cpp
@@ -41,7 +41,10 @@ int main(int argc,char *argv[])
 {
 	int cnt = atoi( argv[1] );
 	int test = atoi( argv[2] );
+	int test2 = atoi( argv[3] );
 	printf("cnt: %d\n", test);
+	printf("test: %d\n", test2);
+
 
 	pthread_t tid[ cnt ];
 	for(int i=0;i<cnt;i++)
